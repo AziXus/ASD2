bf14249167b0ddad9367f9c2f49fed62f639cc4d
Naludrag <stephanecarvalho81@gmail.com>
2019-09-20 20:58:49 +0200

    changement SymbolGraph.h

--- Labo1Ex2/SymbolGraph.h
+++ Labo1Ex2/SymbolGraph.h
@@ -24,6 +24,9 @@ class SymbolGraph
     typedef GraphType Graph; 
 private:
     Graph* g;
+    std::vector<std::string> symbole;
+    typedef std::pair<int,int> Edge;
+    std::vector<Edge> edgeList;
     // A IMPLEMENTER: vos structures privées ici.
     
 public:
@@ -35,12 +38,13 @@ public:
     
     //creation du SymbolGraph a partir du fichier movies.txt
     SymbolGraph(const std::string& filename) {
+        //Graph SymbolGraph;
         /* A IMPLEMENTER */
         // Indication: nous autorisons une double lecture du fichier.
-
-        // exemple de lecture du fichier, ligne par ligne puis element par element (separe par des /)
+        unsigned Film = 0;
+        unsigned Acteur = 1;
         std::string line;
-
+        //Construction du graphe avec les strings
         std::ifstream s(filename);
         while (std::getline(s, line))
         {
@@ -48,31 +52,63 @@ public:
             
             for( auto name : names ) {
                 std::cout << name << " ";
+                if(!contains(name)){
+                    symbole.push_back(name);
+                    edgeList.push_back(std::make_pair(Film,Acteur));
+                    Acteur++;
+                }
+                else{
+                    size_t temp = Acteur;
+                    Acteur = index(name);
+                    edgeList.push_back(std::make_pair(Film,Acteur));
+                    Acteur = temp;
+                }
+                Film = Acteur;
+                Acteur++;
             }
-            
             std::cout << std::endl;
         }
+        /*for(Edge edge : edgeList ){
+            SymbolGraph->addEdge(edge.first,edge.second);
+        }*/
+        // exemple de lecture du fichier, ligne par ligne puis element par element (separe par des /)
+        //Construction du graphe avec les num
+
         s.close();
+        //g = &SymbolGraph;
     }
     
     //verifie la presence d'un symbole
     bool contains(const std::string& name) const {
-        /* A IMPLEMENTER */
+        for(auto name2 : symbole)
+            if(name2 == name)
+                return true;
+        return false;
     }
     
     //index du sommet correspondant au symbole
     int index(const std::string& name) const {
-         /* A IMPLEMENTER */
+         size_t num = 0; 
+         for(auto name2 : symbole){
+            if(name2 == name)
+                return num;
+            num++;
+         }
+        return num;
     }
     
     //symbole correspondant au sommet
     std::string symbol(int idx) const {
-        /* A IMPLEMENTER */
+        return symbole.at(idx);
     }
 
     //symboles adjacents a un symbole
     std::vector<std::string> adjacent(const std::string& name) const {
-        /* A IMPLEMENTER */
+        std::vector<std::string> adj;
+        for(int i : g->adjacent(index(name))){
+            adj.push_back(symbole.at(i));
+        }
+        return adj;
     }
     
     const Graph& G() const {
