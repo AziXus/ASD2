23409e85192c56a14e4d829ba3ce4de10d59779a
Kleeer <48253901+Kleeer@users.noreply.github.com>
2019-10-14 12:20:20 +0200

    Fonctionnel
    supprimé doublon racine cycle

--- Labo2/DirectedCycle.h
+++ Labo2/DirectedCycle.h
@@ -22,107 +22,66 @@ private:
     std::list<int> cycles;
 
     bool foundCycle = false;
-    bool foundSommet = false;
-    int debutCycle;
+    bool cyclingToRoot = false;
+    int cycleRoot;
     std::vector<bool> marked;
     std::vector<bool> stacked;
-    
 
-	void detectCycle(int v) {
-	    //Si v est dans le graphe
+    void detectCycle(int v) {
+        //Si v est dans le graphe
 
-	    marked[v] = true;
-	    stacked[v] = true;
+        marked[v] = true;
+        stacked[v] = true;
 
-	    for (auto w : g->adjacent(v)) {
-	        if(foundCycle){
-	            return;
-                }
-	        else if(!marked[w])
-	            detectCycle(w);
-	        else if(stacked[w]){
-	            foundCycle = true;
-                    cycles.push_back(w);
-                    debutCycle = w;
-                }
-	    }
-            
-            if(v == debutCycle){
-                foundSommet=true;
-                 cycles.push_back(v);
+        for (auto w : g->adjacent(v)) {
+            if(foundCycle){
+                return;
             }
-            
-            if(foundCycle && !foundSommet){
-                cycles.push_back(v);
+            else if(!marked[w])
+                detectCycle(w);
+            else if(stacked[w]){
+                foundCycle = true;
+                cycleRoot = w;
+                cyclingToRoot = true;
             }
-	    stacked[v] = false;
-	}
-	
+        }
+        if(foundCycle && cyclingToRoot){
+            cycles.push_back(v);
+            if(cycleRoot == v){
+                cyclingToRoot = false;
+            }
+        }
+        stacked[v] = false;
+
+    }
+
 public:
-	//constructeur
-	DirectedCycle(const GraphType& g) {
-		/* A IMPLEMENTER */
-		this->g = &g;
+    //constructeur
+    DirectedCycle(const GraphType& g) {
+        /* A IMPLEMENTER */
+        this->g = &g;
 
-		marked.resize(this->g->V(), 0);
-		stacked.resize(this->g->V(), 0);
+        marked.resize(this->g->V(), 0);
+        stacked.resize(this->g->V(), 0);
 
-		for (int v = 0; v < this->g->V(); ++v) {
-                    if(!marked[v])
-                        detectCycle(v);
-                
-                    /*if (foundCycle)
-                        cycles.push_back(v);*/
+        for (int v = 0; v < this->g->V(); ++v) {
+            if(!marked[v]){
+                detectCycle(v);
+            }
+            foundCycle = false;
+        }
+    }
 
-		    foundCycle = false;
-//            std::fill(marked.begin(), marked.end(), false);
-//            std::fill(stacked.begin(), stacked.end(), false);
-		}
-//                bool foundEndCycle = false;
-//                int nbElementCycle = 0;
-//                for(int v : cycles){
-//                    for(auto w : sommets){
-//                        if(v == w){
-//                            foundEndCycle = true;
-//                        }
-//                    }
-//                    nbElementCycle++;
-//                }
-//                for(int i = nbElementCycle; i < cycles.size();){
-//                    cycles.erase(std::next(cycles.begin(), nbElementCycle + 1));
-//                }
-                //cycles.push_front(*(cycles.end()));
-//                bool estCycle = false;
-//                for(int v = 1; v < cycles.size(); ++v){
-//                    for(int w : this->g->adjacent(*std::next(cycles.begin(),v))){
-//                        if(w == *std::next(cycles.begin(),v))
-//                            estCycle = true;
-//                    }
-//                    if(!estCycle){
-//                        cycles.erase(std::next(cycles.begin(),v));
-//                    }
-//                    estCycle = false;
-//                }
-//                for(int v = 0; v < cycles.size(); ++v){
-//                    if(!detectCycle(*std::next(cycles.begin(),v)))
-//                        cycles.erase(std::next(cycles.begin(),v));
-//                }
-	}
-	
-	//indique la presence d'un cycle
-	bool HasCycle() {
-		/* A IMPLEMENTER */
-		//return ...
-		return cycles.size() != 0;
-	}
-	
-	//liste les indexes des sommets formant une boucle
-	std::list<int> Cycle() {
-		/* A IMPLEMENTER */
-		//return ...
-		return cycles;
-	}
-	
+    //indique la presence d'un cycle
+    bool HasCycle() {
+        return cycles.size() != 0;  //TODO return foundCycle -> pb
+    }
+
+    //liste les indexes des sommets formant une boucle
+    std::list<int> Cycle() {
+        return cycles;
+    }
+
 };
 
 #endif
