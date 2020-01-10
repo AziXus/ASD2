9f7b02b458b5703cf226d4b7b0339d44f136cd55
Naludrag <stephanecarvalho81@gmail.com>
2020-01-10 14:59:30 +0100

    Update main.cpp

--- Labo5/main.cpp
+++ Labo5/main.cpp
@@ -12,6 +12,7 @@
  */
 
 #include <string>
+#include <vector>
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -24,18 +25,51 @@ using namespace std;
 
 std::unordered_set<string> set;
 
+vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
+    std::stringstream ss(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+    return elems;
+}
+
+vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, elems);
+    return elems;
+}
+
+vector<std::string> splitInterne(const std::string &s, char delim[], size_t size) {
+    std::vector<std::string> elems;
+    for(int i = 0; i < size; i++){
+        split(s, delim[i], elems);
+    }
+    return elems;
+}
+
 void lectureDonnees(string filename){
         std::string line;
-        
+        char delim[] = {' ', '-'};
+        size_t size = 3;
         std::ifstream s(filename);
         while (std::getline(s, line)) {
-            for(char c : line){
-                if(c != '\'' && !isalpha(c)){
-                    c = ' ';
+            std::vector<string> elements = split(line, ' ');
+            for(string element : elements){
+                std::vector<string> elements2 = split(element, '-');
+                for(string element2 : elements2){
+                    for(char &c : element2){
+                        if(c != '\'' && !isalpha(c)){
+                            if(isdigit(c)){
+                                continue;
+                            }
+                            c = ' ';
+                        }
+                        c = tolower(c);
+                    }
+                    set.insert(element2);
                 }
-                c = tolower(c);
             }
-            set.insert(line);
         }
         s.close();
 }
@@ -54,7 +88,9 @@ void lectureDictio(string filename){
 }
 
 int main(int argc, char** argv) {
-    lectureDictio("dictionary.txt");
+    //lectureDictio("dictionary.txt");
+    
+    lectureDonnees("input_simple.txt");
     
     for(string s : set){
         cout << s << endl;
